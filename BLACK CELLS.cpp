7f9dc7f7cc5b918460d4cbde8b53bf6296f93639
atulsingh7890 <atulsingh7890@gmail.com>
2021-09-02 14:36:03 +0530

    Create BLACK CELLS.cpp

---
+++ BLACK CELLS.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/BLACKCEL
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N = 0;
+	std::cin >> N;
+	std::cout << (N * N)/2 << "\n";
+	return 0;
+}

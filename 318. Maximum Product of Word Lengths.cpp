f85f7e1660fc362e24d85f57d84fd6e610a0eca1
atulsingh7890 <atulsingh7890@gmail.com>
2022-05-29 12:22:47 +0530

    Create 318. Maximum Product of Word Lengths.cpp

---
+++ 318. Maximum Product of Word Lengths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        std::vector<std::pair<int,int>> representations;
+        for(auto & word : words) {
+            int bit_mask_representation = 0;
+            for(auto ch : word) {
+                bit_mask_representation = bit_mask_representation | (1 << (ch-'a'));
+            }
+            representations.push_back({bit_mask_representation, word.size()});
+        }
+        
+        int result = 0;
+        for(int i = 0; i < representations.size(); ++i) {
+            for(int j = i+1; j < representations.size(); ++j) {
+                if((representations[i].first & representations[j].first) == 0) {                    
+                   result = std::max(result, representations[i].second * representations[j].second);
+                }
+            }
+        }
+        return result;
+    }
+};
